d878d1db806d7b8ac0850b623ab8c05183f69528
alisson-lucas <lucasoliveira.alisson@gmail.com>
2021-07-09 22:33:36 -0300

    master | inicio de desenvolvimento

---
+++ index.js
@@ -0,0 +1,9 @@
+let min = 25;
+let seg = 00;
+let pause = 5;
+let sessions = 3
+
+document.getElementById('minutes').innerHTML = min;	
+document.getElementById('pause').innerHTML = pause;	
+document.getElementById('sessions').innerHTML = sessions;	
+
