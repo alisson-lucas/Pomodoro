4614f77a7d02db5510597c4036b5f3f49f159520
alisson-lucas <lucasoliveira.alisson@gmail.com>
2021-07-13 00:17:59 -0300

    master | botoes de tempo

--- index.js
+++ index.js
@@ -7,3 +7,45 @@ document.getElementById('minutes').innerHTML = min;
 document.getElementById('pause').innerHTML = pause;	
 document.getElementById('sessions').innerHTML = sessions;	
 
+function workPlus(){
+    if(min < 60) {
+        min++
+    }
+    document.getElementById('minutes').innerHTML = min;
+}
+
+function workLess(){
+    if (min >= 1){
+        min--
+    }
+    document.getElementById('minutes').innerHTML = min;
+}
+
+function pausePlus(){
+    if(min < 60) {
+        pause++
+    }
+    document.getElementById('pause').innerHTML = pause;
+}
+
+function pauseLess(){
+    if (min >= 1){
+        pause--
+    }
+    document.getElementById('pause').innerHTML = pause;
+}
+
+function sessionPlus(){
+    if(min < 60) {
+        sessions++
+    }
+    document.getElementById('sessions').innerHTML = sessions;
+}
+
+function sessionLess(){
+    if (min >= 1){
+        sessions--
+    }
+    document.getElementById('sessions').innerHTML = sessions;
+}
+
