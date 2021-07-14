c3a5ffd4054f8a318d662ba0c6c981cb9d89177d
alisson-lucas <lucasoliveira.alisson@gmail.com>
2021-07-14 17:35:58 -0300

    master | relogio contando

--- index.js
+++ index.js
@@ -2,23 +2,31 @@ let min = 25;
 let seg = 00;
 let pause = 5;
 let sessions = 3
+let time = localStorage.getItem('time');
 
-document.getElementById('minutes').innerHTML = min;	
-document.getElementById('pause').innerHTML = pause;	
-document.getElementById('sessions').innerHTML = sessions;	
+localStorage.getItem('time', min);
+	
+document.getElementById('pause') ? document.getElementById('pause').innerHTML = pause : '';	
+document.getElementById('sessions') ? document.getElementById('sessions').innerHTML = sessions : '';
+document.getElementById('minutes') ? document.getElementById('minutes').innerHTML = min : '';
+
 
 function workPlus(){
-    if(min < 60) {
-        min++
-    }
+    min < 60 ? min++ : '';
+
+    localStorage.setItem('time', min);
     document.getElementById('minutes').innerHTML = min;
+    document.getElementById('time').innerHTML = min;
+
+    console.log(document.getElementById('time'))
 }
 
 function workLess(){
-    if (min >= 1){
-        min--
-    }
+    min >= 1 ? min-- : '';
+
+    localStorage.setItem('time', min);
     document.getElementById('minutes').innerHTML = min;
+    
 }
 
 function pausePlus(){
@@ -49,3 +57,30 @@ function sessionLess(){
     document.getElementById('sessions').innerHTML = sessions;
 }
 
+
+
+document.getElementById('time') ? document.getElementById('time').innerHTML = '<span>' + time + ':' + seg + '<span>' : '';
+
+
+function relogio(){			
+    if((min > 0) || (seg > 0)){				
+        if(seg == 0){					
+            seg = 59;					
+            min = min - 1	
+        }				
+        else{					
+            seg = seg - 1;				
+        }				
+        if(min.toString().length == 1){					
+            min = "0" + min;				
+        }				
+        if(seg.toString().length == 1){					
+            seg = "0" + seg;				
+        }				
+        document.getElementById('time').innerHTML = min + ":" + seg;				
+        setTimeout('relogio()', 1000);			
+    }			
+    // else{				
+    //     document.getElementById('spanRelogio').innerHTML = "00:00";			
+    // }		
+}	
