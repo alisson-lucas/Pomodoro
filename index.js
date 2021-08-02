eb19827de6e8225c61e8bec3047b2a136749be9a
alisson-lucas <lucasoliveira.alisson@gmail.com>
2021-08-02 18:51:28 -0300

    master | contador de pausa

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ let seg = 00;
 let pause = 5;
 let sessions = 3
 let time = localStorage.getItem('time');
+let pauseTime = localStorage.getItem('pause-time');
+let session = localStorage.getItem('sessions');
 
-localStorage.getItem('time', min);
+localStorage.getItem('time');
+localStorage.getItem('pause-time');
+localStorage.getItem('sessions');
+
+
 	
 document.getElementById('pause') ? document.getElementById('pause').innerHTML = pause : '';	
 document.getElementById('sessions') ? document.getElementById('sessions').innerHTML = sessions : '';
@@ -12,6 +18,8 @@ document.getElementById('minutes') ? document.getElementById('minutes').innerHTM
 
 function loaded() {
     localStorage.setItem('time', min);
+    localStorage.setItem('pause-time', pause);
+    // localStorage.setItem('sessions', sessions);
 }
 
 function workPlus(){
@@ -36,14 +44,18 @@ function pausePlus(){
     if(min < 60) {
         pause++
     }
+    localStorage.setItem('pause-time', pause);
     document.getElementById('pause').innerHTML = pause;
+    console.log(pause);
 }
 
 function pauseLess(){
     if (min >= 1){
         pause--
     }
+    localStorage.setItem('pause-time', pause);
     document.getElementById('pause').innerHTML = pause;
+    console.log(pause);
 }
 
 function sessionPlus(){
@@ -51,6 +63,7 @@ function sessionPlus(){
         sessions++
     }
     document.getElementById('sessions').innerHTML = sessions;
+    localStorage.setItem('sessions', sessions);
 }
 
 function sessionLess(){
@@ -58,12 +71,14 @@ function sessionLess(){
         sessions--
     }
     document.getElementById('sessions').innerHTML = sessions;
+    localStorage.setItem('sessions', sessions);
+
 }
 
 
 
 document.getElementById('time') ? document.getElementById('time').innerHTML = seg === 0 ?  '<span>' + time + ':' + seg + '0 <span>' : '<span>' + time + ':' + seg + '<span>' : '';
-document.getElementById('pause') ? document.getElementById('pause').innerHTML = seg === 0 ?  '<span>' + pause + ':' + seg + '0 <span>' : '<span>' + pause + ':' + seg + '<span>' : '';
+document.getElementById('pause-time') ? document.getElementById('pause-time').innerHTML = seg === 0 ?  '<span>' + pauseTime + ':' + seg + '0 <span>' : '<span>' + pauseTime + ':' + seg + '<span>' : '';
 
 
 function relogio(){			
@@ -83,7 +98,7 @@ function relogio(){
         }				
         document.getElementById('time').innerHTML =  time + ":" + seg;				
         setTimeout('relogio()', 1000);	
-        console.log(time, seg);	
+        // console.log(time, seg);	
     }			
     // else{				
     //     document.getElementById('spanRelogio').innerHTML = "00:00";			
@@ -92,7 +107,43 @@ function relogio(){
         element = document.querySelector('#timer'); 
         element.classList.add('hidden');
 
-        elementPause = document.querySelector('#pause');
-        element.classList.remove('hidden')
+        elementPause = document.querySelector('#pause-timer');
+        elementPause.classList.remove('hidden')
+    }
+}
+
+function pauseCounter(){			
+    if((pauseTime > 0) || (seg > 0)){				
+        if(seg == 0){					
+            seg = 10;					
+            pauseTime = pauseTime - 1	
+        }				
+        else{					
+            seg = seg - 1;				
+        }				
+        if(pauseTime.toString().length == 1){					
+            pauseTime = "0" + pauseTime;				
+        }				
+        if(seg.toString().length == 1){					
+            seg = "0" + seg;				
+        }				
+        document.getElementById('pause-time').innerHTML =  pauseTime + ":" + seg;				
+        setTimeout('pauseCounter()', 1000);	
+        // console.log(pauseTime, seg);	
+    }			
+    // else{				
+    //     document.getElementById('spanRelogio').innerHTML = "00:00";			
+    // }		
+    if (pauseTime == 00 && seg == 00) {
+        // sessions--
+        // localStorage.setItem('sessions', sessions);
+
+        element = document.querySelector('#timer'); 
+        element.classList.remove('hidden');
+
+        elementPause = document.querySelector('#pause-timer');
+        elementPause.classList.add('hidden')
+
+        console.log(sessions);
     }
 }	
